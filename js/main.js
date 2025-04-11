1b830b1028611ba3bb0afeb34de81e22f785a254
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-04-11 10:13:38 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -52,40 +52,45 @@ $(function () {
             });
     }
 });
+console.log("0tes");
 
 // トップページのみ
-if (window.location.pathname.endsWith("index.html")) {
-    // *Swiper
-    const swiper = new Swiper(".swiper", {
-        // Optional parameters
-        loop: true,
+window.addEventListener("DOMContentLoaded", function () {
+    console.log("tes");
+    console.log(window.location.pathname);
+    if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/") {
+        // *Swiper
+        const swiper = new Swiper(".swiper", {
+            // Optional parameters
+            loop: true,
 
-        // If we need pagination
-        pagination: {
-            el: ".swiper-pagination",
-        },
+            // If we need pagination
+            pagination: {
+                el: ".swiper-pagination",
+            },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
-    // swiper キャプション変更
-    const caption = document.querySelector(".c-swiper__caption");
-    swiper.on("slideChangeTransitionEnd", function () {
-        caption.textContent = document.querySelector(".swiper-slide-active").dataset.caption;
-    });
-
-    // scrollBoost
-    if (window.matchMedia("(width > 768px)").matches) {
-        document.querySelectorAll(".c-accordion__scroll").forEach((item) => {
-            new ScrollBooster({
-                viewport: item,
-                scrollMode: "transform",
-                direction: "horizontal",
-            });
+            // Navigation arrows
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
         });
+
+        // swiper キャプション変更
+        const caption = document.querySelector(".c-swiper__caption");
+        swiper.on("slideChangeTransitionEnd", function () {
+            caption.textContent = document.querySelector(".swiper-slide-active").dataset.caption;
+        });
+
+        // scrollBoost
+        if (window.matchMedia("(width > 768px)").matches) {
+            document.querySelectorAll(".c-accordion__scroll").forEach((item) => {
+                new ScrollBooster({
+                    viewport: item,
+                    scrollMode: "transform",
+                    direction: "horizontal",
+                });
+            });
+        }
     }
-}
+});
